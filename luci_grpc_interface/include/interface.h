362815ac6d5f082bcf733d93bb0b9c5b8cc0907a
Christian Prather <christian@luci.com>
2022-06-29 16:20:33 -0600

    Got the node compiling with ros2

--- luci_grpc_interface/include/interface.h
+++ luci_grpc_interface/include/interface.h
@@ -1,5 +1,6 @@
 #include "client.h"
 #include "iostream"
+#include <memory>
 
 #include <geometry_msgs/msg/twist.hpp>
 #include <nav_msgs/msg/odometry.hpp>
@@ -12,8 +13,6 @@
 #include <std_msgs/msg/string.hpp>
 using std::placeholders::_1;
 
-
-
 class Interface : public rclcpp::Node
 {
   private:
@@ -24,32 +23,57 @@ class Interface : public rclcpp::Node
 
     std::vector<std::thread> grpcThreads;
 
+    // std::shared_ptr<rclcpp::Node> node =
+    // rclcpp::Node::make_shared("interface");
+    // rclcpp::Subscription<std_msgs::msg::String> subscriber =
+    //     node->create_subscription<std_msgs::msg::String>(
+    //         "joystick_topic", 1, std::bind(&Interface::sendJSCallback, this,
+    //         _1));
 
-    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("interface");
-    rclcpp::Subscription<std_msgs::msg::String> subscriber = node->create_subscription<std_msgs::msg::String>("joystick_topic", 1, std::bind(&Interface::sendJSCallback, this, _1));
-
-    // std::shared_ptr<rclcpp::Subscription<std_msgs::msg::String>> subscriber = node->create_subscription<std_msgs::msg::String>("joystick_topic", 1, std::bind(&Interface::sendJSCallback, this, _1));
+    // std::shared_ptr<rclcpp::Subscription<std_msgs::msg::String>> subscriber =
+    // node->create_subscription<std_msgs::msg::String>("joystick_topic", 1,
+    // std::bind(&Interface::sendJSCallback, this, _1));
 
     // rclcpp::AsyncSpinner spinner;
 
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+
   public:
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr sensorPublisher;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pidPublisher;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odomPublisher;
+    std::shared_ptr<tf2_ros::TransformBroadcaster> odomBroadcaster;
 
-    // Interface() 
-    // : Node("interface")
-    // {
-    //   subscription_ = this->create_subscription<std_msgs::msg::String>(
-    //     "joystick_topic", 1, std::bind(&Interface::sendJSCallback, this)
-    //   );
-    // }
+    Interface() : Node("interface")
+    {
+        subscription_ = this->create_subscription<std_msgs::msg::String>(
+            "joystick_topic", 1,
+            [this](std_msgs::msg::String::SharedPtr msg) { sendJSCallback(msg); });
 
+        // subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+        //     "/camera/depth/color/points", 10,
+        //     [this](sensor_msgs::msg::PointCloud2::SharedPtr msg) { topic_callback(msg); });
 
-    // rclcpp::Publisher<geometry_msgs::msg::Twist> pidPublisher = node->create_publisher<geometry_msgs::msg::Twist>("chair/cmd_vel", 1);
-    // rclcpp::Publisher::SharedPtr pidPublisher = node->advertise<geometry_msgs::msg::Twist>("chair/cmd_vel", 1);
+        pidPublisher = this->create_publisher<geometry_msgs::msg::Twist>("chair/cmd_vel", 1);
 
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2> sensorPublisher = node->create_publisher<sensor_msgs::msg::PointCloud2>("cloud_in", 1);
-    rclcpp::Publisher<nav_msgs::msg::Odometry> odomPublisher = node->create_publisher<nav_msgs::msg::Odometry>("odom", 1);
+        sensorPublisher = this->create_publisher<sensor_msgs::msg::PointCloud2>("cloud_in", 1);
+        odomPublisher = this->create_publisher<nav_msgs::msg::Odometry>("odom", 1);
+        odomBroadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(this);
 
-    tf2_ros::TransformBroadcaster odomBroadcaster;
+        grpcThreads.emplace_back(&ClientGuide::readAhrsData, luciInterface);
+        grpcThreads.emplace_back(&ClientGuide::readCameraPointData, luciInterface);
+        grpcThreads.emplace_back(&ClientGuide::readEncoderData, luciInterface);
+    }
+
+    // rclcpp::Publisher<geometry_msgs::msg::Twist> pidPublisher =
+    // node->create_publisher<geometry_msgs::msg::Twist>("chair/cmd_vel", 1);
+    // rclcpp::Publisher::SharedPtr pidPublisher =
+    // node->advertise<geometry_msgs::msg::Twist>("chair/cmd_vel", 1);
+
+    // rclcpp::Publisher<sensor_msgs::msg::PointCloud2> sensorPublisher =
+    //     node->create_publisher<sensor_msgs::msg::PointCloud2>("cloud_in", 1);
+    // rclcpp::Publisher<nav_msgs::msg::Odometry> odomPublisher =
+    //     node->create_publisher<nav_msgs::msg::Odometry>("odom", 1);
 
     // Positions for odom
     double x = 0.0;
@@ -58,12 +82,13 @@ class Interface : public rclcpp::Node
 
     rclcpp::Time currentTime, lastTime;
 
-    ClientGuide* luciInterface = new ClientGuide(grpc::CreateChannel(host + ":" + port, grpc::InsecureChannelCredentials()));
+    ClientGuide* luciInterface =
+        new ClientGuide(grpc::CreateChannel(host + ":" + port, grpc::InsecureChannelCredentials()));
 
     // void sendJSCallback(const translator::luci_joystickConstPtr& msg);
-    void sendJSCallback(std_msgs::msg::String);
+    void sendJSCallback(const std_msgs::msg::String::SharedPtr msg);
     void run();
-    Interface();
+    // Interface();
 
     ~Interface();
 };
--- luci_grpc_interface/src/interface.cpp
+++ luci_grpc_interface/src/interface.cpp
@@ -1,30 +1,37 @@
 #include "../include/interface.h"
 #include "../include/differentiator.h"
+
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
 std::vector<std::thread> processThreads;
 
 // #include <spdlog/spdlog.h>
 
-// ClientGuide* luciInterface = new ClientGuide(grpc::CreateChannel(host + ":" + port, grpc::InsecureChannelCredentials()));
-// std::cout << "Client created at " << host << port << std::endl;
+// ClientGuide* luciInterface = new ClientGuide(grpc::CreateChannel(host + ":" + port,
+// grpc::InsecureChannelCredentials())); std::cout << "Client created at " << host << port <<
+// std::endl;
 
-void Interface::sendJSCallback(const translator::luci_joystickConstPtr& msg)
+// void Interface::sendJSCallback(const translator::luci_joystickConstPtr& msg)
+void Interface::sendJSCallback(const std_msgs::msg::String::SharedPtr msg)
 {
     // std::cout << "Recieved js val: " << msg->forwardBack << " " << msg->leftRight << std::endl;
 
-    this->luciInterface->sendJS(msg->forwardBack + 100, msg->leftRight + 100);
+    // this->luciInterface->sendJS(msg->forwardBack + 100, msg->leftRight + 100);
+    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
 }
 
-Interface::Interface() : spinner(4)
-{
-    grpcThreads.emplace_back(&ClientGuide::readAhrsData, luciInterface);
-    grpcThreads.emplace_back(&ClientGuide::readCameraPointData, luciInterface);
-    // grpcThreads.emplace_back(&ClientGuide::readRadarPointData, luciInterface);
-    // grpcThreads.emplace_back(&ClientGuide::readUltrasonicPointData, luciInterface);
+// Interface::Interface() : spinner(4)
+// {
+//     grpcThreads.emplace_back(&ClientGuide::readAhrsData, luciInterface);
+//     grpcThreads.emplace_back(&ClientGuide::readCameraPointData, luciInterface);
+//     // grpcThreads.emplace_back(&ClientGuide::readRadarPointData, luciInterface);
+//     // grpcThreads.emplace_back(&ClientGuide::readUltrasonicPointData, luciInterface);
 
-    grpcThreads.emplace_back(&ClientGuide::readEncoderData, luciInterface);
+//     grpcThreads.emplace_back(&ClientGuide::readEncoderData, luciInterface);
 
-    spinner.start();
-}
+//     spinner.start();
+// }
 
 float degreesTraveled(float current, float previous, bool leftWheel)
 {
@@ -65,18 +72,30 @@ float degreesTraveled(float current, float previous, bool leftWheel)
     return degreeDelta;
 }
 
+auto createQuaternionMsgFromYaw(double yaw)
+{
+    tf2::Quaternion q;
+    q.setRPY(0, 0, yaw);
+    return tf2::toMsg(q);
+}
+
 int main(int argc, char** argv)
 {
 
-    ros::init(argc, argv, "interface");
+    rclcpp::init(argc, argv);
 
-    Interface* interface = new Interface();
-    ros::Rate rate(20);
+    // TODO: figure out spin rate stuff
+    auto interfaceNode = std::make_shared<Interface>();
+    rclcpp::spin(interfaceNode);
 
+    rclcpp::Rate loop_rate(20);
+
+    // Interface* interface = new Interface();
+    // ros::Rate rate(20);
 
     // CHECK THE POINTCLOUD TO MAKE SURE ITS RIGHT WAY UP
     // interface->luciInterface->activateAutoMode();
-    interface->luciInterface->activateUserMode();
+    interfaceNode->luciInterface->activateUserMode();
 
     // processThreads.emplace_back(&Interface::run, interface);
 
@@ -90,14 +109,14 @@ int main(int argc, char** argv)
     float totalDegreesLeft = 0.0;
     float totalDegreesRight = 0.0;
 
-    interface->currentTime = ros::Time::now();
-    interface->lastTime = ros::Time::now();
+    // interface->currentTime = ros::Time::now();
+    // interface->lastTime = ros::Time::now();
 
-    while (ros::ok())
+    while (rclcpp::ok())
     {
-        interface->currentTime = ros::Time::now();
+        // interface->currentTime = ros::Time::now();
 
-        auto encoderData = interface->luciInterface->getEncoderData();
+        auto encoderData = interfaceNode->luciInterface->getEncoderData();
         auto leftAngle = encoderData.leftAngle;
         auto rightAngle = encoderData.rightAngle;
         float encoderTimestamp = encoderData.timestamp;
@@ -134,22 +153,22 @@ int main(int argc, char** argv)
         float rightVelocity = rightDiff.differentiate(rightDistanceMeters, encoderTimestamp);
 
         // Point cloud processing
-        auto cameraPointData = interface->luciInterface->getCameraPointCloud();
+        auto cameraPointData = interfaceNode->luciInterface->getCameraPointCloud();
         // auto radarPointData = interface->luciInterface->getRadarPointCloud();
         // auto ultrasonicPointData = interface->luciInterface->getUltrasonicPointCloud();
         // auto fullPointCloud = cameraPointData + radarPointData + ultrasonicPointData;
         auto fullPointCloud = cameraPointData;
-        sensor_msgs::PointCloud2 rosPointCloud;
+        sensor_msgs::msg::PointCloud2 rosPointCloud;
         pcl::toROSMsg(fullPointCloud, rosPointCloud);
-        std_msgs::Header header;
+        std_msgs::msg::Header header;
         header.frame_id = "base_camera";
-        header.stamp = interface->currentTime;
+        header.stamp = interfaceNode->currentTime;
         rosPointCloud.header = header;
-        interface->sensorPublisher.publish(rosPointCloud);
+        interfaceNode->sensorPublisher->publish(rosPointCloud);
         // std::cout << "Points : " << pointData.size() << std::endl;
 
         // AHRS data processing
-        auto ahrsData = interface->luciInterface->getAhrsData();
+        auto ahrsData = interfaceNode->luciInterface->getAhrsData();
 
         // Odometry for navigation / mapping
 
@@ -160,46 +179,50 @@ int main(int argc, char** argv)
 
         // std::cout << "VX: " << vx << " VTH: " << vth << std::endl;
         // NOT SURE ABOUT MIXING TIME STAMPS HERE?? (ENCODER STREAM VS ROS)
-        double dt = (interface->currentTime - interface->lastTime).toSec();
-        // double deltaX = (ahrsData.linear_velocity.x * cos(interface->th) - ahrsData.linear_velocity.y * sin(interface->th)) * dt;
-        // double deltaY = (ahrsData.linear_velocity.x * sin(interface->th) + ahrsData.linear_velocity.y * cos(interface->th)) * dt;
-        // double deltaTh = ahrsData.angular_velocity.z * dt;
-        double deltaX = (vx * cos(interface->th) - vy * sin(interface->th)) * dt;
-        double deltaY = (vx * sin(interface->th) + vy * cos(interface->th)) * dt;
+
+        // double dt = (interfaceNode->currentTime - interfaceNode->lastTime).toSec();
+        double dt = 1.0;
+
+        // double deltaX = (ahrsData.linear_velocity.x * cos(interface->th) -
+        // ahrsData.linear_velocity.y * sin(interface->th)) * dt; double deltaY =
+        // (ahrsData.linear_velocity.x * sin(interface->th) + ahrsData.linear_velocity.y *
+        // cos(interface->th)) * dt; double deltaTh = ahrsData.angular_velocity.z * dt;
+        double deltaX = (vx * cos(interfaceNode->th) - vy * sin(interfaceNode->th)) * dt;
+        double deltaY = (vx * sin(interfaceNode->th) + vy * cos(interfaceNode->th)) * dt;
         double deltaTh = vth * dt;
 
-        interface->x += deltaX;
-        interface->y += deltaY;
-        interface->th += deltaTh;
+        interfaceNode->x += deltaX;
+        interfaceNode->y += deltaY;
+        interfaceNode->th += deltaTh;
 
         // PID loop for driving from nav
-        geometry_msgs::Twist pidUpdateMsg;
+        geometry_msgs::msg::Twist pidUpdateMsg;
         pidUpdateMsg.linear.y = vy;
         pidUpdateMsg.linear.x = vx;
         pidUpdateMsg.angular.z = vth;
-        interface->pidPublisher.publish(pidUpdateMsg);
+        interfaceNode->pidPublisher->publish(pidUpdateMsg);
 
-        geometry_msgs::Quaternion odomQuat = tf::createQuaternionMsgFromYaw(interface->th);
-        geometry_msgs::TransformStamped odomTrans;
-        odomTrans.header.stamp = interface->currentTime;
+        geometry_msgs::msg::Quaternion odomQuat = createQuaternionMsgFromYaw(interfaceNode->th);
+        geometry_msgs::msg::TransformStamped odomTrans;
+        odomTrans.header.stamp = interfaceNode->currentTime;
         odomTrans.header.frame_id = "odom";
         odomTrans.child_frame_id = "base_link";
 
-        odomTrans.transform.translation.x = interface->x;
-        odomTrans.transform.translation.y = interface->y;
+        odomTrans.transform.translation.x = interfaceNode->x;
+        odomTrans.transform.translation.y = interfaceNode->y;
         odomTrans.transform.translation.z = 0.0;
         odomTrans.transform.rotation = odomQuat;
 
         // Send transform to tf
-        interface->odomBroadcaster.sendTransform(odomTrans);
+        interfaceNode->odomBroadcaster->sendTransform(odomTrans);
 
-        nav_msgs::Odometry odom;
-        odom.header.stamp = interface->currentTime;
+        nav_msgs::msg::Odometry odom;
+        // odom.header.stamp = interface->currentTime;
         odom.header.frame_id = "odom";
 
         // Set position
-        odom.pose.pose.position.x = interface->x;
-        odom.pose.pose.position.y = interface->y;
+        odom.pose.pose.position.x = interfaceNode->x;
+        odom.pose.pose.position.y = interfaceNode->y;
         odom.pose.pose.position.z = 0.0;
         odom.pose.pose.orientation = odomQuat;
 
@@ -209,11 +232,11 @@ int main(int argc, char** argv)
         odom.twist.twist.linear.y = vy;
         odom.twist.twist.angular.z = vth;
 
-        interface->odomPublisher.publish(odom);
-        interface->lastTime = interface->currentTime;
+        interfaceNode->odomPublisher->publish(odom);
+        // interface->lastTime = interface->currentTime;
 
-        ros::spinOnce();
-        rate.sleep();
+        rclcpp::spin_some(interfaceNode);
+        loop_rate.sleep();
     }
 
     // interface->run();
