7f095418279ae8e4f20c879c4fd52743a2178399
Christian Prather <clemsbach@gmail.com>
2024-07-12 12:18:28 -0600

    Shrunk proto file

--- luci_grpc_interface/client/include/client/client.h
+++ luci_grpc_interface/client/include/client/client.h
@@ -3,7 +3,19 @@
  *
  * The main connection point to the Luci safety system
  *
- * @copyright Copyright (c) 2023 LUCI Mobility, Inc. All Rights Reserved.
+ * @copyright Copyright 2024 LUCI Mobility, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #pragma once
@@ -31,13 +43,12 @@
 #include <vector>
 
 using sensors::AhrsData;
+using sensors::CameraMetaData;
 using sensors::CameraPoints;
-using sensors::ChairSpeed;
 using sensors::JoystickData;
 using sensors::NavigationScaling;
 using sensors::RadarPoints;
 using sensors::UltrasonicDistances;
-using sensors::CameraMetaData;
 
 namespace Luci::ROS2
 {
@@ -83,8 +94,7 @@ class ClientGuide
         std::shared_ptr<DataBuffer<EncoderData>> encoderDataBuff,
         std::shared_ptr<DataBuffer<CameraIrData>> irDataBuffLeft,
         std::shared_ptr<DataBuffer<CameraIrData>> irDataBuffRight,
-        std::shared_ptr<DataBuffer<CameraIrData>> irDataBuffRear,
-        int initialFrameRate);
+        std::shared_ptr<DataBuffer<CameraIrData>> irDataBuffRear, int initialFrameRate);
 
     /**
      * @brief Destroy the Client Guide object
--- luci_grpc_interface/client/src/client.cpp
+++ luci_grpc_interface/client/src/client.cpp
@@ -1,7 +1,19 @@
 /**
  * @file client.cpp
  *
- * @copyright Copyright (c) 2022 Patroness, LLC. All Rights Reserved.
+ * @copyright Copyright 2024 LUCI Mobility, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -19,7 +31,6 @@ using Luci::ROS2::ClientGuide;
 
 using sensors::DriveMode;
 using sensors::ModeCtrl;
-using sensors::RampMode;
 using sensors::RemoteJsValues;
 using sensors::Response;
 
